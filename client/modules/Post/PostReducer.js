86f0c0adaa7120c3ca0b13153801019dd2d7461a
Adam Więcek <adamw28@gmail.com>
2018-03-07 19:50:54 +0100

    Add project

--- client/modules/Post/PostReducer.js
+++ client/modules/Post/PostReducer.js
@@ -1,21 +1,30 @@
-import { ADD_POST, ADD_POSTS, DELETE_POST } from './PostActions';
+import { ADD_POST, EDIT_POST, ADD_POSTS, DELETE_POST } from './PostActions';
 
 // Initial State
 const initialState = { data: [] };
 
 const PostReducer = (state = initialState, action) => {
   switch (action.type) {
-    case ADD_POST :
+    case ADD_POST:
       return {
         data: [action.post, ...state.data],
       };
 
-    case ADD_POSTS :
+    case ADD_POSTS:
       return {
         data: action.posts,
       };
 
-    case DELETE_POST :
+    case EDIT_POST:
+      return {
+        data: state.data.map(post => {
+          return post.cuid === action.cuid
+            ? Object.assign({}, post, action.post)
+            : post;
+        }),
+      };
+
+    case DELETE_POST:
       return {
         data: state.data.filter(post => post.cuid !== action.cuid),
       };
@@ -31,7 +40,8 @@ const PostReducer = (state = initialState, action) => {
 export const getPosts = state => state.posts.data;
 
 // Get post by cuid
-export const getPost = (state, cuid) => state.posts.data.filter(post => post.cuid === cuid)[0];
+export const getPost = (state, cuid) =>
+  state.posts.data.filter(post => post.cuid === cuid)[0];
 
 // Export Reducer
 export default PostReducer;
